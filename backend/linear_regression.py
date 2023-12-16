d63c1dc72fdde99edbc91ab7b02247e2e2e44379
Anas8e <94750646+Anas8e@users.noreply.github.com>
2023-12-16 09:08:09 +0100

    Landing page

---
+++ backend/linear_regression.py
@@ -0,0 +1,6 @@
+# Python script for linear regression prediction
+
+# Function for linear regression prediction based on financial data
+def linear_regression_prediction(data):
+    # Python code for linear regression prediction
+    pass  # Placeholder for the actual implementation
--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // This function ensures the code within runs after the DOM is fully loaded
-    
-    // Handling the click event for the Explore Portfolio button
-    const exploreBtn = document.querySelector('.btn');
-    exploreBtn.addEventListener('click', function(event) {
-        event.preventDefault();
-        // Perform actions when the Explore Portfolio button is clicked
-        
-        // For example, scroll smoothly to the portfolio section
-        const portfolioSection = document.getElementById('portfolio');
-        portfolioSection.scrollIntoView({ behavior: 'smooth' });
-    });
-    
-    // Add more functionalities or event listeners here as needed
-    // For instance, handling other interactions or events on the website
-});
+// JavaScript for interactive functionalities
+
+// Function to fetch financial data
+function fetchFinancialData() {
+    // Code for fetching financial data
+}
+
+// Function to display predictions using linear regression
+function displayPredictions(data) {
+    // Code to showcase predictions
+}
+
+// Event listeners or other functionalities as needed
